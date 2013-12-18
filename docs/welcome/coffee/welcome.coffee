43f509f6bd97b0d36ef955270d88a64bc9001ea1
Adam Schwartz <aschwartz@hubspot.com>
2013-12-17 22:07:31 -0500

    Create a welcome page

--- docs/welcome/coffee/welcome.coffee
+++ docs/welcome/coffee/welcome.coffee
@@ -2,20 +2,23 @@ isMobile = $(window).width() < 567
 
 init = ->
     setupHero()
-    setupBrowserDemo()
 
 setupHero = ->
-    $target = $('.tether-target-demo')
+    $target = $('.tooltip-target-demo')
 
     positions = [
         'top left'
         'left top'
+        'left middle'
         'left bottom'
         'bottom left'
+        'bottom center'
         'bottom right'
         'right bottom'
+        'right middle'
         'right top'
         'top right'
+        'top center'
     ]
 
     if isMobile
@@ -31,14 +34,15 @@ setupHero = ->
     for position in positions
         drops[position] = new Drop
             target: $target[0]
-            className: 'drop-tooltip-theme-arrows'
+            className: 'tooltip-theme-default'
             attach: position
-            constrainToScrollParent: true
+            constrainToScrollParent: false
+            constrainToWindow: false
             openOn: ''
             content: $.map(position.split(' '), (word) -> word.substr(0, 1).toUpperCase() + word.substr(1)).join(' ')
 
         # TODO - remove once zackbloom fixes
-        drops[position].$drop.addClass "drop-attached-#{ position.replace(' ', '-')}"
+        drops[position].$drop.addClass "drop-attached-#{ position.replace(' ', '-') }"
 
     openIndex = 0
     frames = 0
@@ -69,102 +73,5 @@ setupHero = ->
 
     else
         openNextDrop()
-
-setupBrowserDemo = ->
-    $browserDemo = $('.browser-demo.showcase')
-
-    $startPoint = $('.browser-demo-start-point')
-    $stopPoint = $('.browser-demo-stop-point')
-
-    $iframe = $('.browser-window iframe')
-    $browserContents = $('.browser-content .browser-demo-inner')
-
-    $sections = $('.browser-demo-section')
-
-    $('body').append """
-        <style>
-            table.showcase.browser-demo.fixed-bottom {
-                top: #{ $sections.length }00%
-            }
-        </style>
-    """
-
-    $(window).scroll ->
-        scrollTop = $(window).scrollTop()
-
-        if $startPoint.position().top < scrollTop and scrollTop + window.innerHeight < $stopPoint.position().top
-            $browserDemo.removeClass('fixed-bottom')
-            $browserDemo.addClass('fixed')
-
-            $sections.each ->
-                $section = $ @
-
-                if $section.position().top < scrollTop < $section.position().top + $section.outerHeight()
-                    setSection $section.data('section')
-
-                return true
-
-        else
-            $browserDemo.removeAttr('data-section')
-            $browserDemo.removeClass('fixed')
-
-            if scrollTop + window.innerHeight > $stopPoint.position().top
-                $browserDemo.addClass('fixed-bottom')
-            else
-                $browserDemo.removeClass('fixed-bottom')
-
-    $iframe.load ->
-        iframeWindow = $iframe[0].contentWindow
-
-        $items = $iframe.contents().find('.item')
-
-        $items.each (i) ->
-            $item = $(@)
-
-            drop = new iframeWindow.Drop
-                target: $item[0]
-                className: 'drop-theme-arrows'
-                attach: 'right top'
-                constrainToWindow: true
-                openOn: 'click'
-                content: '<div class="drop-demo-spacer"></div>'
-
-            $item.data('drop', drop)
-
-            # TODO - remove once zackbloom fixes
-            drop.$drop.addClass "drop-attached-right-top"
-
-    setSection = (section) ->
-        $browserDemo.attr('data-section', section)
-
-        $('.section-copy').removeClass('active')
-        $(""".section-copy[data-section="#{ section }"]""").addClass('active')
-
-        openExampleItem = ->
-            if isMobile
-                $iframe.contents().find('.item:first').data().drop.open()
-            else
-                $iframe.contents().find('.item:eq(2)').data().drop.open()
-
-        closeAllItems = ->
-            $iframe.contents().find('.item').each -> $(@).data().drop.close() or true
-
-        switch section
-
-            when 'intro'
-                closeAllItems()
-                openExampleItem()
-
-            when 'explain'
-                closeAllItems()
-                openExampleItem()
-
-            when 'resize'
-                closeAllItems()
-                openExampleItem()
-
-            when 'outro'
-                closeAllItems()
-                openExampleItem()
 
 init()
--- docs/welcome/js/welcome.js
+++ docs/welcome/js/welcome.js
@@ -1,17 +1,16 @@
 (function() {
-  var init, isMobile, setupBrowserDemo, setupHero;
+  var init, isMobile, setupHero;
 
   isMobile = $(window).width() < 567;
 
   init = function() {
-    setupHero();
-    return setupBrowserDemo();
+    return setupHero();
   };
 
   setupHero = function() {
     var $target, frameLengthMS, frames, openAllDrops, openIndex, openNextDrop, position, positions, _i, _len;
-    $target = $('.tether-target-demo');
-    positions = ['top left', 'left top', 'left bottom', 'bottom left', 'bottom right', 'right bottom', 'right top', 'top right'];
+    $target = $('.tooltip-target-demo');
+    positions = ['top left', 'left top', 'left middle', 'left bottom', 'bottom left', 'bottom center', 'bottom right', 'right bottom', 'right middle', 'right top', 'top right', 'top center'];
     if (isMobile) {
       positions = ['top left', 'bottom left', 'bottom right', 'top right'];
     }
@@ -20,9 +19,10 @@
       position = positions[_i];
       drops[position] = new Drop({
         target: $target[0],
-        className: 'drop-tooltip-theme-arrows',
+        className: 'tooltip-theme-default',
         attach: position,
-        constrainToScrollParent: true,
+        constrainToScrollParent: false,
+        constrainToWindow: false,
         openOn: '',
         content: $.map(position.split(' '), function(word) {
           return word.substr(0, 1).toUpperCase() + word.substr(1);
@@ -62,92 +62,6 @@
     } else {
       return openNextDrop();
     }
-  };
-
-  setupBrowserDemo = function() {
-    var $browserContents, $browserDemo, $iframe, $sections, $startPoint, $stopPoint, setSection;
-    $browserDemo = $('.browser-demo.showcase');
-    $startPoint = $('.browser-demo-start-point');
-    $stopPoint = $('.browser-demo-stop-point');
-    $iframe = $('.browser-window iframe');
-    $browserContents = $('.browser-content .browser-demo-inner');
-    $sections = $('.browser-demo-section');
-    $('body').append("<style>\n    table.showcase.browser-demo.fixed-bottom {\n        top: " + $sections.length + "00%\n    }\n</style>");
-    $(window).scroll(function() {
-      var scrollTop;
-      scrollTop = $(window).scrollTop();
-      if ($startPoint.position().top < scrollTop && scrollTop + window.innerHeight < $stopPoint.position().top) {
-        $browserDemo.removeClass('fixed-bottom');
-        $browserDemo.addClass('fixed');
-        return $sections.each(function() {
-          var $section;
-          $section = $(this);
-          if (($section.position().top < scrollTop && scrollTop < $section.position().top + $section.outerHeight())) {
-            setSection($section.data('section'));
-          }
-          return true;
-        });
-      } else {
-        $browserDemo.removeAttr('data-section');
-        $browserDemo.removeClass('fixed');
-        if (scrollTop + window.innerHeight > $stopPoint.position().top) {
-          return $browserDemo.addClass('fixed-bottom');
-        } else {
-          return $browserDemo.removeClass('fixed-bottom');
-        }
-      }
-    });
-    $iframe.load(function() {
-      var $items, iframeWindow;
-      iframeWindow = $iframe[0].contentWindow;
-      $items = $iframe.contents().find('.item');
-      return $items.each(function(i) {
-        var $item, drop;
-        $item = $(this);
-        drop = new iframeWindow.Drop({
-          target: $item[0],
-          className: 'drop-theme-arrows',
-          attach: 'right top',
-          constrainToWindow: true,
-          openOn: 'click',
-          content: '<div class="drop-demo-spacer"></div>'
-        });
-        $item.data('drop', drop);
-        return drop.$drop.addClass("drop-attached-right-top");
-      });
-    });
-    return setSection = function(section) {
-      var closeAllItems, openExampleItem;
-      $browserDemo.attr('data-section', section);
-      $('.section-copy').removeClass('active');
-      $(".section-copy[data-section=\"" + section + "\"]").addClass('active');
-      openExampleItem = function() {
-        if (isMobile) {
-          return $iframe.contents().find('.item:first').data().drop.open();
-        } else {
-          return $iframe.contents().find('.item:eq(2)').data().drop.open();
-        }
-      };
-      closeAllItems = function() {
-        return $iframe.contents().find('.item').each(function() {
-          return $(this).data().drop.close() || true;
-        });
-      };
-      switch (section) {
-        case 'intro':
-          closeAllItems();
-          return openExampleItem();
-        case 'explain':
-          closeAllItems();
-          return openExampleItem();
-        case 'resize':
-          closeAllItems();
-          return openExampleItem();
-        case 'outro':
-          closeAllItems();
-          return openExampleItem();
-      }
-    };
   };
 
   init();
